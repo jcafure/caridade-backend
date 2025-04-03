7f9fbfec856dd011247c23a1d686021686b784cb
jcafure <cafure.jaime@gmail.com>
2025-04-03 12:08:12 -0400

    #Correção PullRequest: Correção de teste que quebrou no PR.

--- src/test/java/dev/caridadems/controller/CharityGroupControllerTest.java
+++ src/test/java/dev/caridadems/controller/CharityGroupControllerTest.java
@@ -14,7 +14,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
