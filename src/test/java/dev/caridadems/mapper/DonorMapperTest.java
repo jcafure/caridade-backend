74bf5323f676811b850b93ee73ca3c7a1c21ffe3
jcafure <cafure.jaime@gmail.com>
2025-04-24 14:09:23 -0400

    #Melhorias: Feito teste unitario para validação do sonar.

---
+++ src/test/java/dev/caridadems/mapper/DonorMapperTest.java
@@ -0,0 +1,38 @@
+package dev.caridadems.mapper;
+
+import dev.caridadems.dto.DonorRegisterDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class DonorMapperTest {
+
+    @InjectMocks
+    private DonorMapper donorMapper;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void dtoToEntity() {
+        final var dto = new DonorRegisterDto();
+        dto.setEmail("admin@caridade.com");
+        dto.setName("admin");
+        dto.setPhone("(67) 99999-9999");
+        dto.setExternalId(123);
+
+        final var donor = donorMapper.dtoToEntity(dto);
+
+        assertNotNull(donor);
+        assertEquals(dto.getEmail(), donor.getEmail());
+        assertEquals(dto.getName(), donor.getName());
+        assertEquals(dto.getPhone(), donor.getPhone());
+        assertEquals(dto.getExternalId(), donor.getExternalId());
+    }
+}
