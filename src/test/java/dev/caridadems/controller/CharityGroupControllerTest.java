2a6af8086abd4edcc950d6eaf3db93580e2cd62e
jcafure <cafure.jaime@gmail.com>
2025-05-05 13:39:22 -0400

    #Funcionalidade: Criado profile de test para os testes de controllers. Criado SecurityConfig para permissão de recursos oauth.
    #Atualização de dependencias: Adicionado spring-security/oauth

--- src/test/java/dev/caridadems/controller/CharityGroupControllerTest.java
+++ src/test/java/dev/caridadems/controller/CharityGroupControllerTest.java
@@ -5,11 +5,14 @@ import dev.caridadems.dto.AddressDTO;
 import dev.caridadems.dto.CharityGroupDTO;
 import dev.caridadems.service.CharityGroupService;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -24,7 +27,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest(CharityGroupController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class CharityGroupControllerTest {
 
     @Autowired
