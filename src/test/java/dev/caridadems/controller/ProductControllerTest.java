bddef78bf2397ee558046277d174ac5c49505930
jcafure <cafure.jaime@gmail.com>
2025-05-12 15:07:24 -0400

    #Merge: Correção de issues sonae. develop to main;

--- src/test/java/dev/caridadems/controller/ProductControllerTest.java
+++ src/test/java/dev/caridadems/controller/ProductControllerTest.java
@@ -9,7 +9,6 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
